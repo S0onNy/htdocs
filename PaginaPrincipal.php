3557f48b4474b468be1834937c5258a51ccc07e0
Vega <40494980+lamision@users.noreply.github.com>
2018-06-22 11:22:57 -0400

    Add files via upload
    EDICION PAG PRINCIPAL

---
+++ PaginaPrincipal.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Carousel Template for Bootstrap</title>
+
+    <!-- Bootstrap CSS carpeta-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+
+ 
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+  </head>
+
+<body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Project name</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+ 
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    
+
+
+      <!-- Listado de las canchas. -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Cancha El Trebol</h2>
+          <p class="lead">Dirección: Corvi Nueve 4034-4066, Valdivia, Región de los Ríos<br>
+Horario:<br>
+viernes	8:00–18:00<br>
+sábado	Cerrado<br>
+domingo	Cerrado<br>
+lunes	8:00–18:00<br>
+martes	8:00–18:00<br>
+miércoles	8:00–18:00<br>
+jueves	8:00–18:00.</p>
+          <a href="recinto1.php">Arrendar</a>
+        </div>
+
+        
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="Imagenes/Canchas/Cancha 1.jpg">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">Coliseo Municipal Antonio Azurmendy Riveros</h2>
+          <p class="lead">El Coliseo Municipal de Valdivia o Coliseo Municipal Antonio Azurmendy Riveros llamado "la Catedral del Básquetbol"​ fue Inaugurado en 1966 con ocasión del histórico Campeonato Mundial Extraordinario.<br>
+Dirección: Av Pedro Montt esquina Errázuriz,Valdivia, Región de los Ríos
+Horario: <br>
+viernes	4:00–0:00<br>
+sábado	Abierto las 24 horas<br>
+domingo	0:00–4:00<br>
+lunes	4:00–0:00<br>
+martes	Abierto las 24 horas<br>
+miércoles	Abierto las 24 horas<br>
+jueves	0:00–4:00.</p>
+          <a href="recinto1.php">Arrendar</a>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="Imagenes/Canchas/Cancha 2.png">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">CANCHAS SINTETICAS SIMPSON</h2>
+          <p class="lead">Dirección: AV.SIMPSON 364 BARRIO ESTACION, Av. Simpson 364, Valdivia, Región de los Ríos
+Horario: <br>
+viernes	10:00–0:00<br>
+sábado	10:00–0:00<br>
+domingo	10:00–0:00<br>
+lunes	10:00–0:00<br>
+martes	10:00–0:00<br>
+miércoles	10:00–0:00<br>
+jueves	10:00–0:00.</p>
+          <a href="recinto1.php">Arrendar</a>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500" src="Imagenes/Canchas/cancha 3.jpg">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>© 2016 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/holder.min.js"></script>
+    
+  
+
+<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs><style type="text/css"></style></defs><text x="0" y="25" style="font-weight:bold;font-size:25pt;font-family:Arial, Helvetica, Open Sans, sans-serif">500x500</text></svg></body>
+</html>
